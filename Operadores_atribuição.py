6a0a4eedf1be3dc8ec55902f5af3bcbaeacc2481
Ivan Fontes De Assis Santos <162522430+Ivan-Fontes-de-Assis-Santos@users.noreply.github.com>
2024-10-18 18:08:21 -0300

    Add files via upload

---
+++ Operadores_atribuição.py
@@ -0,0 +1,27 @@
+#Saldo que vamos usar para atibuir valores usando como base "Operadores aritmértricos":
+Saldo= 500
+print(Saldo)
+
+Saldo += 200
+print(Saldo)
+
+Saldo -= 200
+print(Saldo)
+
+Saldo /= 200
+print(Saldo)
+
+Saldo //= 200
+print(Saldo)
+
+Saldo *= 200
+print(Saldo)
+
+Saldo %= 200
+print(Saldo)
+
+Saldo *= 200
+print(Saldo)
+
+Saldo **= 200
+print(Saldo)
