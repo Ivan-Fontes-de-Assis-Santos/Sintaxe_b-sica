ad025b8f9968336725c225559c300884323dcdf4
Ivan Fontes De Assis Santos <162522430+Ivan-Fontes-de-Assis-Santos@users.noreply.github.com>
2024-10-24 15:12:32 -0300

    Add files via upload

---
+++ Operadores_lógicos.py
@@ -0,0 +1,17 @@
+Saldo= 1000
+Saque= 200
+Limite= 100
+#Usando os Operadores de Comparação como base:
+print(Saldo >= Saque)
+print(Saque <= Limite)
+
+#Este é o Operador "E" ou "and":
+print(Saldo >= Saque and Saque <= Limite)
+#Este é o Operador "OU" ou "or":
+print(Saldo >= Saque or Saque <= Limite)
+#Este é o Operador de negação "not":
+contatos_emergencia= []
+print(not 1000 > 1500)
+print(not contatos_emergencia)
+print(not "saque 1500")
+print("")
